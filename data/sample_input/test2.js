c5460a81472b5f0e194a871630453273f46de1de
Makoto Inoue <makoto@new-bamboo.co.uk>
2009-01-10 14:35:37 +0000

    Added sample files

--- data/sample_input/test2.js
+++ data/sample_input/test2.js
@@ -1,10 +1,7 @@
 $(document).ready(function(){
 console.log(1);
 //console.log( $('td:contains("日付：")').html().match(/日付：12月＼d{1,2}日/)[0]);
-console.log( $('td:contains("日付：")').html().match(/日付：12月＼d{1,2}日/)[0].replace(/日付：/, ""))
+日付：12月＼d{1,2}日
+console.log( $('td:contains("日付：")').html().match()[0].replace(/日付：/, ""))
 }
 )
-
-for (var i = obj.length - 1; i >= 0; i--){
-  obj[i]
-};
