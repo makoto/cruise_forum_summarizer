08475df898ee71378edc71ebf27a3bdd779a27f2
Makoto Inoue <makoto@new-bamboo.co.uk>
2009-01-10 17:20:10 +0000

    Refactored into Thread class

--- lib/keiji_parser.rb
+++ lib/keiji_parser.rb
@@ -9,15 +9,27 @@ class Page
   end
 end
 
-class Topic
-  attr_reader :thread_id, :title, :date, :user, :context, :comments
+class Thread 
+  attr_reader :thread_id, :title, :date, :user, :context
+  
   def initialize(element)
     input = element.search('input[@name=del]').first
     @thread_id = input.attributes['value']
     @title =  NKF.nkf("-w", input.parent.inner_text).delete(@thread_id + "．")
-    @user =   NKF.nkf("-w", element.search('tr')[2].inner_text).slice(/名前：(.*) 日付/, 1).gsub(/\?/,"").strip
-    @date = NKF.nkf("-w", element.search('tr')[2].inner_text).slice(/日付：(.*$)/, 1)
 
+    @user =   NKF.nkf("-w", element.search('tr')[@name_and_date_position].inner_text).slice(/名前：(.*) 日付/, 1).gsub(/\?/,"").strip
+    @date = NKF.nkf("-w", element.search('tr')[@name_and_date_position].inner_text).slice(/日付：(.*$)/, 1)
+  end
+end
+
+class Topic < Thread
+  attr_reader :comments
+
+  def initialize(element)
+    @name_and_date_position = 2
+    
+    super(element)
+    
     context_lines = element.search('table')[1].search('td')[0].inner_html.split("\n")
     contexts = []
     context_lines.each{|c| break if c.match(/<table/); contexts << c}
@@ -26,16 +38,12 @@ class Topic
   end
 end
 
-class Comment
-  attr_reader :thread_id, :title, :date, :user, :context
+class Comment < Thread
   
   def initialize(element)
-    input = element.search('input[@name=del]').first
-    @thread_id = input.attributes['value']
-    @title =  NKF.nkf("-w", input.parent.inner_text).delete(@thread_id + "．")
-
-    @user =   NKF.nkf("-w", element.search('tr')[1].inner_text).slice(/名前：(.*) 日付/, 1).gsub(/\?/,"").strip
-    @date = NKF.nkf("-w", element.search('tr')[1].inner_text).slice(/日付：(.*$)/, 1)
+    @name_and_date_position = 1
+    
+    super(element)
 
     @context = NKF.nkf("-w", element.search('tr')[2].search('td')[0].inner_html).strip
   end
